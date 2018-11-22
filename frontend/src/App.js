bccf05b693e86f3cff2cebd1fdd8090eeed7cbf1
mahmoud <mahmoud16h@gmail.com>
2018-11-22 23:45:13 +0000

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import './App.css';
+import {getMerchantData, getProductData} from './actions/data'
+import ProductCard from './components/ProductCard'
+import FilterHeader from './components/FilterHeader'
+
+class App extends Component {
+
+    componentDidMount() {
+        this.props.getMerchants()
+        this.props.getProducts()
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <div className='logo-container'>
+                    <img className='logo' alt="" src={require('./floom_logo.jpg')} />
+                </div>
+                <FilterHeader/>
+                <div className='line-break'/>
+                <ProductCard data={this.props.user}/>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    user: state.data,
+});
+
+const mapDispatchToProps = {
+    getMerchants: getMerchantData,
+    getProducts: getProductData,
+}
+
+export default connect(
+    mapStateToProps, mapDispatchToProps
+)(App);
