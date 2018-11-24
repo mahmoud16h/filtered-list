2442ef147dd4ca468ef09560fddd3795247c58f2
mahmoud <mahmoud16h@gmail.com>
2018-11-24 17:16:03 +0000

    fixed coordinates function

--- frontend/src/reducers/data.js
+++ frontend/src/reducers/data.js
@@ -1,23 +1,21 @@
 const initialState = {}
 
-const distance = (lat1, lon1, lat2, lon2) => {
-    if ((lat1 === lat2) && (lon1 === lon2)) {
-        return 0;
-    }
-    else {
-        let radlat1 = Math.PI * lat1/180;
-        let radlat2 = Math.PI * lat2/180;
-        let theta = lon1-lon2;
-        let radtheta = Math.PI * theta/180;
-        let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        if (dist > 1) {
-            dist = 1;
-        }
-        dist = Math.acos(dist);
-        dist = dist * 180/Math.PI;
-        dist = dist * 1.609344
-        return dist;
-    }
+const distance = (lat1,lon1,lat2,lon2) => {
+    let R = 6371; // Radius of the earth in km
+    let dLat = deg2rad(lat2-lat1);  // deg2rad below
+    let dLon = deg2rad(lon2-lon1);
+    let a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    let d = R * c; // Distance in km
+    return d;
+}
+
+const deg2rad = (deg) => {
+    return deg * (Math.PI/180)
 }
 
 export const data = (state = initialState, action) => {
